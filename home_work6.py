95102d4238c2e167b7786ee0c7eda9d1594ff418
DashaZava <112952295+DashaZava@users.noreply.github.com>
2023-08-12 12:26:49 +0100

    Update home_work6.py
    Renewed

--- home_work6.py
+++ home_work6.py
@@ -2,11 +2,8 @@ import os
 import shutil
 import zipfile
 
-# Функция для нормализации имени файла
-
-
+# Функція для нормалізації імені файлу
 def normalize(name):
-    # Транслитерация кириллических символов и замена неподходящих символов на "_"
     normalized_name = []
     for char in name:
         if char.isalpha() or char.isdigit() or char == ' ':
@@ -15,13 +12,10 @@ def normalize(name):
             normalized_name.append('_')
     return ''.join(normalized_name)
 
-# Функция для обработки файлов разных типов
-
-
+# Функція для обробки файлів різних типів
 def process_file(file_path, destination_folder):
     _, ext = os.path.splitext(file_path)
-    # Убираем точку из расширения и переводим в верхний регистр
-    ext = ext[1:].upper()
+    ext = ext[1:].upper()  # Видаляємо точку з розширення і переводимо в верхній регістр
 
     if ext in ['JPEG', 'PNG', 'JPG', 'SVG']:
         dest_folder = os.path.join(destination_folder, 'images')
@@ -34,48 +28,44 @@ def process_file(file_path, destination_folder):
     elif ext in ['ZIP', 'GZ', 'TAR']:
         dest_folder = os.path.join(destination_folder, 'archives', os.path.splitext(
             os.path.basename(file_path))[0])
-        # Распаковка архива
+        # Розпаковка архіву
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
             zip_ref.extractall(dest_folder)
-        return  # Не перемещаем распакованные файлы
+        return  # Не переміщуємо розпаковані файли
     else:
         dest_folder = os.path.join(destination_folder, 'unknown')
 
-    # Создаем папку назначения, если ее нет
+    # Створюємо папку призначення, якщо її немає
     os.makedirs(dest_folder, exist_ok=True)
 
-    # Имя файла после нормализации
+    # Ім'я файлу після нормалізації
     normalized_file_name = normalize(
         os.path.splitext(os.path.basename(file_path))[0])
     new_file_path = os.path.join(dest_folder, f"{normalized_file_name}{ext}")
 
-    # Перемещение файла
+    # Переміщення файлу
     shutil.move(file_path, new_file_path)
 
-# Функция для обработки папки
-
-
+# Функція для обробки папки
 def process_folder(folder_path):
     for root, _, files in os.walk(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             process_file(file_path, folder_path)
-        # Удаление пустых папок
+        # Видалення порожніх папок
         if not os.listdir(root):
             os.rmdir(root)
 
-# Основная функция
-
-
+# Основна функція
 def main(source_folder):
-    # Создаем папку для архивов, если ее нет
+    # Створюємо папку для архівів, якщо її немає
     archives_folder = os.path.join(source_folder, "archives")
     os.makedirs(archives_folder, exist_ok=True)
 
-    # Получаем список всех файлов и папок в указанной директории
+    # Отримуємо список всіх файлів і папок у вказаній директорії
     items = os.listdir(source_folder)
 
-    # Обрабатываем каждый элемент (файл или папку)
+    # Обробляємо кожен елемент (файл або папку)
     for item in items:
         item_path = os.path.join(source_folder, item)
         if os.path.isfile(item_path):
@@ -83,19 +73,17 @@ def main(source_folder):
         elif os.path.isdir(item_path) and item not in ["archives", "video", "audio", "documents", "images"]:
             process_folder(item_path)
 
-
-# Если скрипт запущен как отдельный файл
 if __name__ == "__main__":
- import sys
+    import sys
 
-    # Проверяем количество аргументов командной строки
+    # Перевіряємо кількість аргументів командного рядка
     if len(sys.argv) != 2:
         print("Usage: python sort.py /path/to/folder")
         sys.exit(1)
 
     source_folder = sys.argv[1]
 
-    # Проверяем, существует ли указанная папка
+    # Перевіряємо, чи існує вказана папка
     if not os.path.exists(source_folder):
         print("Source folder does not exist.")
         sys.exit(1)
