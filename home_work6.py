93904de15c0131b2356af3106698009e0d5751a0
DashaZava <112952295+DashaZava@users.noreply.github.com>
2023-08-12 15:29:47 +0100

    Update home_work6.py

--- home_work6.py
+++ home_work6.py
@@ -2,6 +2,15 @@ import os
 import shutil
 import zipfile
 
+# Словник з дозволеними розширеннями та відповідними папками
+valid_extensions = {
+    'images': ['JPEG', 'PNG', 'JPG', 'SVG'],
+    'video': ['AVI', 'MP4', 'MOV', 'MKV'],
+    'documents': ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'],
+    'audio': ['MP3', 'OGG', 'WAV', 'AMR'],
+    'archives': ['ZIP', 'GZ', 'TAR']
+}
+
 # Функція для нормалізації імені файлу
 def normalize(name):
     normalized_name = []
@@ -17,30 +26,20 @@ def process_file(file_path, destination_folder):
     _, ext = os.path.splitext(file_path)
     ext = ext[1:].upper()  # Видаляємо точку з розширення і переводимо в верхній регістр
 
-    if ext in ['JPEG', 'PNG', 'JPG', 'SVG']:
-        dest_folder = os.path.join(destination_folder, 'images')
-    elif ext in ['AVI', 'MP4', 'MOV', 'MKV']:
-        dest_folder = os.path.join(destination_folder, 'video')
-    elif ext in ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']:
-        dest_folder = os.path.join(destination_folder, 'documents')
-    elif ext in ['MP3', 'OGG', 'WAV', 'AMR']:
-        dest_folder = os.path.join(destination_folder, 'audio')
-    elif ext in ['ZIP', 'GZ', 'TAR']:
-        dest_folder = os.path.join(destination_folder, 'archives', os.path.splitext(
-            os.path.basename(file_path))[0])
-        # Розпаковка архіву
-        with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(dest_folder)
-        return  # Не переміщуємо розпаковані файли
-    else:
+    dest_folder = None
+    for folder, extensions in valid_extensions.items():
+        if ext in extensions:
+            dest_folder = os.path.join(destination_folder, folder)
+            break
+
+    if dest_folder is None:
         dest_folder = os.path.join(destination_folder, 'unknown')
 
     # Створюємо папку призначення, якщо її немає
     os.makedirs(dest_folder, exist_ok=True)
 
     # Ім'я файлу після нормалізації
-    normalized_file_name = normalize(
-        os.path.splitext(os.path.basename(file_path))[0])
+    normalized_file_name = normalize(os.path.splitext(os.path.basename(file_path))[0])
     new_file_path = os.path.join(dest_folder, f"{normalized_file_name}{ext}")
 
     # Переміщення файлу
@@ -52,6 +51,7 @@ def process_folder(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             process_file(file_path, folder_path)
+
         # Видалення порожніх папок
         if not os.listdir(root):
             os.rmdir(root)
@@ -70,7 +70,7 @@ def main(source_folder):
         item_path = os.path.join(source_folder, item)
         if os.path.isfile(item_path):
             process_file(item_path, source_folder)
-        elif os.path.isdir(item_path) and item not in ["archives", "video", "audio", "documents", "images"]:
+        elif os.path.isdir(item_path) and item not in valid_extensions.keys() and item != "archives":
             process_folder(item_path)
 
 if __name__ == "__main__":
